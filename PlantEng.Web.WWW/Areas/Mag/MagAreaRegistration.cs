e4ea49e945a278dbad28283d83960ac13a5c2e16
baifang.xing <baifang.xing@qunar.com>
2015-04-07 12:04:03 +0800

    add mag

---
+++ PlantEng.Web.WWW/Areas/Mag/MagAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PlantEng.Web.WWW.Areas.Mag
+{
+    public class MagAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Mag";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Mag_default",
+                "Mag/{action}",
+                new { action = "Index", id = UrlParameter.Optional, controller="Home" }
+            );
+        }
+    }
+}
